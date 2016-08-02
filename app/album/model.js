a31ec6c47f0275dad90ec5dd9fb6a5b150978e75
RoobScoobs <iruben229@gmail.com>
2016-08-02 15:25:55 -0400

    Import belongsTo to create association in model

--- app/album/model.js
+++ app/album/model.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
+import { belongsTo } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
   artist: DS.attr('string'),
   user: DS.belongsTo('user'),
-  genre: DS.belongsTo('genre'),
+  // genre: DS.belongsTo('genre'),
 });
