718d666fffb4cc719c79a4f456ad43bab8e2d906
RoobScoobs <iruben229@gmail.com>
2016-08-02 09:23:40 -0400

    Define create and redirect action in the route
    Binding actions in the template

--- app/new-album/route.js
+++ app/new-album/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+
+    createAlbum (data) {
+      let album = this.get('store').createRecord('album', data);
+      return album.save()
+      .then(() => this.transitionTo('albums'));
+      // .then(() => this.get('flashMessages').success('New building added'))
+      // .catch(() => {
+      //   this.get('flashMessages')
+      //   .danger('There was a problem. Please try again.');
+      // });
+    },
+
+    goToAlbums () {
+      this.transitionTo('albums');
+    }
+  }
 });
