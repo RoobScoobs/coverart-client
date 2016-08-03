0c26a9a65ea58eca06c1901f0f7ca78cb06eaf3e
RoobScoobs <iruben229@gmail.com>
2016-08-03 11:08:38 -0400

    Add delete and edit actions in the route

--- app/album/route.js
+++ app/album/route.js
@@ -4,4 +4,16 @@ export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('album', params.album_id);
   },
+
+  actions: {
+
+    deleteAlbum (album) {
+      album.destroyRecord()
+      .then(() => this.transitionTo('albums'));
+    },
+
+    editAlbum (album) {
+      this.transitionTo('edit-album', album);
+    },
+  },
 });
