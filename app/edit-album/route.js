c8e2d35cd952d9f066f88da03c67d01fabc3b583
RoobScoobs <iruben229@gmail.com>
2016-08-03 11:09:42 -0400

    Edit album route has update function
    Not working in its current state; only function transitions to /albums

--- app/edit-album/route.js
+++ app/edit-album/route.js
@@ -4,4 +4,22 @@ export default Ember.Route.extend({
   model (params) {
     return this.get('store').findRecord('album', params.album_id);
   },
+
+  actions: {
+    editAlbum (data) {
+      this.get('store').findRecord('album', data.id)
+           .then(function(album) {
+             album.title = data.title;
+             album.artist = data.artist;
+             album.description = data.description;
+             console.log(album);
+             return album.save();
+           })
+           .then(() => this.transitionTo('albums'));
+         },
+
+    goToAlbums () {
+      this.transitionTo('albums');
+    },
+  },
 });
