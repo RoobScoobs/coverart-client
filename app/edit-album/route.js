f7f1d1385541f194309b2fd650c6970e6dcce0ff
RoobScoobs <iruben229@gmail.com>
2016-08-03 18:41:30 -0400

    Edit album action detailed in route
    Bind data from object passed in from the component to the specific
    album we want to update, includes all three attributes

--- app/edit-album/route.js
+++ app/edit-album/route.js
@@ -2,21 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model (params) {
-    return this.get('store').findRecord('album', params.album_id);
+    return this.get('store').findRecord('album', params.id);
   },
 
   actions: {
     editAlbum (data) {
       this.get('store').findRecord('album', data.id)
-           .then(function(album) {
-             album.title = data.title;
-             album.artist = data.artist;
-             album.description = data.description;
-             console.log(album);
-             return album.save();
-           })
-           .then(() => this.transitionTo('albums'));
-         },
+      .then((album) => {
+        album.title = data.title;
+        album.artist = data.artist;
+        album.description = data.description;
+        album.save();
+      })
+       .then(() => this.transitionTo('albums'));
+     },
 
     goToAlbums () {
       this.transitionTo('albums');
