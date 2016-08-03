0decdb6eb0d506fa965090d2c6d948fc88e8d51c
RoobScoobs <iruben229@gmail.com>
2016-08-03 18:42:56 -0400

    Add transition to all albums page on Submit action

--- app/components/new-album-form/component.js
+++ app/components/new-album-form/component.js
@@ -8,6 +8,7 @@ export default Ember.Component.extend({
   actions: {
    submit () {
      this.sendAction('createAlbum', this.get('album'));
+     this.sendAction('goToAlbums');
    },
 
    reset () {
