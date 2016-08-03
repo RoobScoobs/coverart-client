fb79108f44ad26848cbbde94cbd503822e51954c
RoobScoobs <iruben229@gmail.com>
2016-08-03 11:11:38 -0400

    Add edit action at the component level

--- app/components/edit-album-form/component.js
+++ app/components/edit-album-form/component.js
@@ -2,4 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: ['form'],
+
+  album: {},
+
+  actions: {
+    edit () {
+      this.sendAction('editAlbum', this.get('album'));
+    },
+
+    reset () {
+      this.sendAction('goToAlbums');
+    },
+  },
 });
