65f8c4912554aefd7baab91c50c6018940e5ca5b
RoobScoobs <iruben229@gmail.com>
2016-08-03 18:33:09 -0400

    Route includes delete and edit actions
    Edit action transitions to edit-album route

--- app/album/route.js
+++ app/album/route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model (params) {
-    return this.get('store').findRecord('album', params.album_id);
+    return this.get('store').findRecord('album', params.id);
   },
 
   actions: {
